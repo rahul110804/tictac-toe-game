128aa2f241c347df55bf7bd2b7de7ddbca3188c2
rahul110804 <rahul.110804@gmail.com>
2024-07-06 16:07:08 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+let reset_btn=document.querySelector("#reset-btn");
+let boxes=document.querySelectorAll(".box");
+let restart_btn=document.querySelector("#restart-btn");
+let msg_container=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turno=true;
+
+const winpatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const restartGame = () =>
+    {
+        turno=true;
+        enableBoxes();
+        msg_container.classList.add("hide");
+    }
+
+boxes.forEach((box)=>
+{
+    box.addEventListener("click", () =>
+    {
+        if(turno)
+            {
+                box.innerText="O";
+                turno=false;
+            }
+            else
+            {
+                box.innerText="X";
+                turno=true;
+            }
+            box.disabled=true;
+
+            checkWinner();
+    });
+});
+
+const disableBoxes = () =>
+    {
+        for(let box of boxes)
+            {
+                box.disabled=true;
+            }
+    };
+    const enableBoxes = () =>
+        {
+            for(let box of boxes)
+                {
+                    box.disabled=false;
+                    box.innerText="";
+                }
+        };
+
+const showWinner=(winner)=>
+    {
+        msg.innerText=`congratulations ,Winner is ${winner}`;
+        msg_container.classList.remove("hide");
+        disableBoxes();
+    };
+const checkWinner = () =>
+    {
+        for(let pattern of winpatterns)
+            {
+                    let pos1Val=boxes[pattern[0]].innerText;
+                    let pos2Val=boxes[pattern[1]].innerText;
+                    let pos3Val=boxes[pattern[2]].innerText;
+
+                    if(pos1Val!="" && pos2Val!="" && pos3Val!="")
+                        {
+                            if(pos1Val== pos2Val && pos2Val==pos3Val)
+                                {
+                                    console.log("winner",pos1Val);
+                                    showWinner(pos1Val);
+                                }
+                        }
+            }
+    };
+        reset_btn.addEventListener("click",restartGame);
+        restart_btn.addEventListener("click",restartGame);
